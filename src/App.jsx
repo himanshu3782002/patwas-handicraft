55fa4cc027a8b2f5a0963d8b3183e611f08c7b7b
AyushRawat2122 <ayushprovalorant@gmail.com>
2025-04-26 20:29:25 +0530

    project initalized

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>
+        Hii This is himanshu chouhan project...
+      </h1>
+    </div>
+  );
+}
+// jsx js + xml (js + html) class <-
+export default App;
